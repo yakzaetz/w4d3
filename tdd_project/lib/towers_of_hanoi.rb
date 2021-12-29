a97014b181e8e96333f8e7c30d0cb289d4f2bb4d
yakzaetz <yakzaetz@gmail.com>
2021-12-29 17:22:00 -0500

    won? method complete

--- tdd_project/lib/towers_of_hanoi.rb
+++ tdd_project/lib/towers_of_hanoi.rb
@@ -1,3 +1,6 @@
 class TowersOfHanoi
-        
+    attr_reader :towers
+    def initialize
+        @towers = [[5,4,3,2,1], [], []]
+    end
 end
--- tdd_project/spec/towers_of_hanoi_spec.rb
+++ tdd_project/spec/towers_of_hanoi_spec.rb
@@ -2,16 +2,27 @@ require "rspec"
 require "towers_of_hanoi"
 
 describe TowersOfHanoi do 
-    subject(:ToH) { TowersOfHanoi.new }
+    subject(:toh) { TowersOfHanoi.new }
 
     describe "#initialize" do
-        it "should display board of a 2-D array. Each array should be unique in memory." do
+        # it "should display board of a 2-D array. Each array should be unique in memory." do
 
-        end
+        # end
 
         it "#towers should display towers" do
-            
+            expect(toh.towers).to be_instance_of(Array)
         end
     end
+
+    describe "won?" do
+        it "game over if" do
+            toh.towers[0] = []
+            toh.towers[2] = [5,4,3,2,1]
+            # toh.towers = [[], [], [5,4,3,2,1]]
+            expect(toh.towers).to eq([[], [], [5,4,3,2,1]])
+        end 
+    end
+    
+    
 
 end 
