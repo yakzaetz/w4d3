75310581b279e05c84f4e6c3d9e7fb706493ccd1
egumin <ethangumin@gmail.com>
2021-12-29 14:46:45 -0500

    create spec for two_sum

--- tdd_project/lib/tdd.rb
+++ tdd_project/lib/tdd.rb
@@ -1,5 +1,20 @@
 # require
 
 def my_uniq(arr)
-    arr = []
+    narr = []
+    arr.each do |element|
+        if narr.include?(element)
+            next
+        else
+            narr << element
+        end
+    end
+    narr
+end
+
+def two_sum(arr)
+    res = []
+    (0...arr.length).each do |i|
+        (i + 1...arr.length).each do |i2|
+            res << [i, i2] if arr[i] + arr[i2] == 0
 end
--- tdd_project/spec/tdd_spec.rb
+++ tdd_project/spec/tdd_spec.rb
@@ -2,15 +2,16 @@ require "rspec"
 require "tdd"
 
 describe "my_uniq" do
-    context "array is empty" do 
-        it "should return an empty array" do
-            expect(my_uniq([])).to eq([])
-        end 
-    end 
-
-    subject(:array){Array.new(1,2,1,3,3)}
     it "should return an array with all elements once" do
         array = [1,2,1,3,3]
         expect(my_uniq(array)).to eq([1,2,3])
     end
+end
+
+describe "two_sum" do
+    it "should return pair arrays inside of a pairs array of the indices of which when added together sum to zero." do
+        array = [-1, 0, 2, -2, 1]
+        expect(two_sum(array)).to eq([[0, 4], [2, 3]])
+    end
+
 end
