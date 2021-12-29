7463dd2d70721a9f68188fe6979563329f995f74
egumin <ethangumin@gmail.com>
2021-12-29 17:03:09 -0500

    continue towers of hanoi

--- tdd_project/spec/towers_of_hanoi_spec.rb
+++ tdd_project/spec/towers_of_hanoi_spec.rb
@@ -2,5 +2,16 @@ require "rspec"
 require "towers_of_hanoi"
 
 describe TowersOfHanoi do 
+    subject(:ToH) { TowersOfHanoi.new }
+
+    describe "#initialize" do
+        it "should display board of a 2-D array. Each array should be unique in memory." do
+
+        end
+
+        it "#towers should display towers" do
+            
+        end
+    end
 
 end 
